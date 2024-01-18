7755400e91f10e39ae18c4170f1ee18344915179
Abhinav Srivastava <atg271@gmail.com>
2024-01-18 00:21:49 -0500

    wip

--- src/common/compute.ts
+++ src/common/compute.ts
@@ -18,16 +18,25 @@ class InternalSocket {
     }
 }
 
+interface Options {
+    party_id?: string;
+    secret_key?: any;
+    public_key?: any;
+    __internal_socket?: InternalSocket;
+    [key: string]: any; // for additional properties
+}
+
 function create_computation_instance
-    (jiff: typeof JiffClient, computation_id: string, options: any): any {
+    (jiff: typeof JiffClient, computation_id: string, options: Options): any {
     options = Object.assign({}, options);
     options.party_id = 's1';
 
     options.secret_key = null;
     options.public_key = null;
-
-    options.__internal_socket = new Client('<server_instance>', computation_id, options);
-
+    options.__internal_socket = new InternalSocket(jiff, computation_id);
+    var computation_instance = new JiffClient(options.party_id, computation_id, options);
+    
+    // @TODO - Figure out where the server member in jiff codebase is
     computation_instance.server = jiff; 
 
     return computation_instance;
--- src/jiffClient.ts
+++ src/jiffClient.ts
@@ -1,13 +1,15 @@
-// to be used as jiff: client.jiffClient;
-export function JiffClient(hostname: string, computation_id: string, options: any): any {
-    options = Object.assign({}, options);
-    options.party_id = 's1';
+export class JiffClient {
+    hostname: string;
+    computation_id: string;
+    options: any;
+    server: any;
 
-    options.secret_key = null;
-    options.public_key = null;
-
-    options.__internal_socket = new Client(hostname, computation_id, options);
-
-    // @ts-ignore
-    return computation_instance;
+    constructor(hostname: string, computation_id: string, options: any) {
+        this.hostname = hostname;
+        this.computation_id = computation_id;
+        this.options = Object.assign({}, options);
+        this.options.party_id = 's1';
+        this.options.secret_key = null;
+        this.options.public_key = null;
+    }
 }
