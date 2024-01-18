c8fc75022c1074b0ae88396cf6e3d4b9b01ea3ff
Abhinav Srivastava <atg271@gmail.com>
2024-01-18 04:02:51 -0500

    wip

--- src/jiffClient.ts
+++ src/jiffClient.ts
@@ -3,6 +3,8 @@ export class JiffClient {
     computation_id: string;
     options: any;
     server: any;
+    helpers: any; // keeps compiler happy, we don't want `any` types anywhere ideally
+    share_helpers: any; 
 
     constructor(hostname: string, computation_id: string, options: any) {
         this.hostname = hostname;
@@ -11,5 +13,6 @@ export class JiffClient {
         this.options.party_id = 's1';
         this.options.secret_key = null;
         this.options.public_key = null;
+        this.helpers = {};
     }
 }
