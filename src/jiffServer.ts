c6e07c337ec406e255de9d0edd8a22eb8b93572b
Abhinav Srivastava <atg271@gmail.com>
2024-01-18 00:27:44 -0500

    wip

--- src/jiffServer.ts
+++ src/jiffServer.ts
@@ -3,3 +3,21 @@ import * as $ from 'jquery-deferred';
 
 import * as helpers from './common/helpers';
 import * as computer from './common/compute';
+
+
+interface ComputationMaps {
+    clientIds: { [key: string]: string[] };
+    spareIds: { [key: string]: any };
+    maxCount: { [key: string]: number };
+    keys: { [key: string]: { [partyId: string]: string } };
+    secretKeys: { [key: string]: string };
+    freeParties: { [key: string]: { [partyId: string]: boolean } };
+  }
+  
+  interface SocketMaps {
+    socketId: { [key: string]: string };
+    computationId: { [key: string]: string };
+    partyId: { [key: string]: string };
+  }
+
+  
